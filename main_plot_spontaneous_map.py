ee7bc31d70586acb39fcc221df69c63c74496853
Radoslav Jochman <jochmanr@natur.cuni.cz>
2025-04-07 16:22:27 +0200

    Changed naming of results for spontaneous plotting.

--- main_plot_spontaneous_map.py
+++ main_plot_spontaneous_map.py
@@ -6,7 +6,7 @@ import os
 import argparse
 from array_analysis import load_object
 import plotting
-from helper import extract_pickle_paths
+from helper import extract_paths
 
 parser = argparse.ArgumentParser(description="Generate spontaneous map graphs using array objects and a reference object with specified principal components.")
 parser.add_argument("--array_obj_dir", default=None, type=str, help="Directory path containing the array object(s) to be processed.")
@@ -15,16 +15,17 @@ parser.add_argument("--PCs", default=None, type=str, help="Comma-separated princ
 parser.add_argument("--result_dir", default=None, type=str, help="Directory where the generated graph will be saved.")
 
 def main(args):
-    objects_paths = extract_pickle_paths(args.array_obj_dir)
+    objects_paths = extract_paths(args.array_obj_dir)
     PC1, PC2 = map(int, args.PCs.split(","))
     ref_obj = load_object(args.ref_path)
     ref_obj.compute_new_PC(PC1, PC2)
     for path in objects_paths:
         file_name = os.path.splitext(os.path.basename(path))[0]
+        sample = path.split("/")[-2]
         arr_obj = load_object(path)
         arr_obj.compute_new_PC(PC1, PC2)
         p = plotting.ggplot_spontaneous_map(arr_obj, ref_obj)
-        result_path = f"{args.result_dir}/spont_{file_name}"
+        result_path = f"{args.result_dir}/spont_{sample}_{file_name}.png"
         p.save(filename=result_path, width=8, height=8, dpi=300)
 
 if __name__ == '__main__':

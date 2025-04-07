54755c1847b92a0615bd2ce759073cb2d1077caf
Radoslav Jochman <jochmanr@natur.cuni.cz>
2025-04-07 15:46:02 +0200

    Changed parameter name.

--- main_generate_rmse_dist_param.py
+++ main_generate_rmse_dist_param.py
@@ -8,13 +8,13 @@ parser.add_argument("--data_location", default=None, type=str, help="Path to a d
 parser.add_argument("--ref_obj_path", default=None, type=str, help="Path to a object used as the reference for RMSE computation")
 parser.add_argument("--result_dir", default=None, type=str, help="Directory where output results will be stored.")
 parser.add_argument("--PCs", default=None, type=str, help="Principal Components to use in the analysis (comma-separated).")
-parser.add_argument("--result_name", default=None, type=str, help="Name of the graph.")
+parser.add_argument("--sample_name", default=None, type=str, help="Name of the sample.")
 
 def main(args: argparse.Namespace):
-    paths = helper.extract_pickle_paths(args.data_location)
+    paths = helper.extract_paths(args.data_location, ".pkl")
     PCs = list(map(int,args.PCs.split(",")))
-    rmse_dist = helper.calculate_rmse_distr_for_sample(paths,PCs,args.ref_obj_path,"Sample_1")
-    rmse_dist.to_csv(f"{args.result_dir}/{args.result_name}")
+    rmse_dist = helper.calculate_rmse_distr_for_sample(paths,PCs,args.ref_obj_path,args.sample_name)
+    rmse_dist.to_csv(f"{args.result_dir}/{args.sample_name}.csv",index=False)
 if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
